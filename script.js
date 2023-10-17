af2491e714378bba94e2516e9fc89c7b6b2acdcb
kvk98 <119598315+kvk98@users.noreply.github.com>
2023-10-17 11:24:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-//your code here
-
+// your code here
+let arr = ['The Virupaksha Temple', 'A Victoria Memorial', 'An Tajmahal'];
+let withoutArticles =  [] ;
+let map = {} ;
+for(let i = 0 ; i < arr.length; i++) {
+    let str = arr[i];
+    str = str.replace(/\ban\b|\bthe\b|\ba\b/gi, "")
+    str = str.replace(/ +/gi, " ");
+    str = str.trim();
+    map[str] = arr[i];
+    withoutArticles.push(str);
+}
+withoutArticles.sort();
+for(let i = 0; i < withoutArticles.length; i++) {
+    console.log(map[ withoutArticles[i] ]);
+}
